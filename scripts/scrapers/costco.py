4ed544f95b8efb902d7db5a9f65ba8970b2b417a
Ezra Zibel <168735774+ezibbler@users.noreply.github.com>
2025-03-19 19:59:46 -0400

    scrapes all products from costco site

--- scripts/scrapers/costco.py
+++ scripts/scrapers/costco.py
@@ -3,26 +3,25 @@ from bs4 import BeautifulSoup
 import re
 
 def __main__():
-    #gets html of main costco page
-    values = find_links("https://costcofdb.com/food-database", "https://costcofdb.com/product-category")
+    #gets all products in the costco database
+    #TODO: get prices from costco site and add them into csv
+    products = recursive_get("https://costcofdb.com/food-database", "https://costcofdb.com/product-category")
 
-    print(join(values, joiner="\n"))
+def find_links(origin : str, target : str, final : bool = False):
+    #gets target links from base link
 
-def join(list, joiner = ""):
-    joined = ""
-    for element in list:
-        joined += str(element) + joiner
-    return joined
-
-def find_links(link : str, target : str):
-    r = requests.get(link)
+    r = requests.get(origin)
     regexed_target = target.replace("/", "\/").replace(".", "\.")
 
     soup = BeautifulSoup(r.content, "html.parser")
     soup = soup.prettify()
 
     #finds links to categories
-    links = re.findall(f"href=\"({regexed_target}\/[^\/\"]+?)\"", soup)
+    if final:
+        #gets list of products
+        links = re.findall(f"href=\"(https:\/\/costcofdb\.com\/product\/[^\/\"]+?)\"", soup)
+    else:
+        links = re.findall(f"aria-label=\"[^\/\"]+?\" href=\"({regexed_target}\/[^\/\"]+?)\"", soup)
 
     #find unique values
     values = []
@@ -32,6 +31,22 @@ def find_links(link : str, target : str):
         else:
             values.append(link)
     return values
+
+def recursive_get(origin : str, target : str):
+    #get each link on page
+    links = find_links(origin, target)
+
+    #if there are no subcategory links
+    if links == []:
+        #get the product links
+        return find_links(origin, origin, final=True)
+    else:
+        #get the links contained in all subpages and add them to a list
+        products = []
+        for link in links:
+            print(link)
+            products.extend(recursive_get(link, link))
+        return products
 
 if __name__ == "__main__":
     __main__()
